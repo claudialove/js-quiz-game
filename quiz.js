6b095f0fb0f4a2d237e7bc67edea87932b8f97b3
Claudia Petty <claudialove32@gmail.com>
2020-04-10 10:09:56 -0400

    timer works but not perfect

--- quiz.js
+++ quiz.js
@@ -2,17 +2,13 @@
 
 //the following elements will need to be SLECTED in order to be MANIPULATED
 
-//on title screen this element is clicked to start the game which starts decrement of time
-var startButton = document.getElementById("#start-button"); 
 
 //play game screen this element changes text to display each of the questions in the array that will be written containting a nested object for each question with a questions and a correct answer
-var question = document.getElementById("#question");
+var question = document.getElementById("question");
 
-//on play game screen - each #question has a list of multiple choice (li tags): one i correct and 2 are wrong - this element will need to change with hover over, and trigger and event to decide correct or incorrect and loop to the next question or end game if all questionsed have been looped through
+//on play game screen - each #question has a list of multiple choice (li tags): one is correct and 2 are wrong - this element will need to change with hover over, and trigger and event to decide correct or incorrect and loop to the next question or end game if all questions have been looped through
 var lis = document.querySelectorAll("li");
 
-//on play game screen a time displays and counts down the seconds in a minute, it continues this pace for correct answers and subntracts 10 seconds for wrong answers - when timer runs out the game is over
-var timer = document.getElementById("#timer");
 
 //on the all done screen the game is back in game over mode and this screen indicates the user answered all questions before the timer ran out - this screen has a span - the logic will have to store the score in an object and return this value to display in the span that starts as 0
 var score = document.getElementsByClassName("score");
@@ -21,10 +17,95 @@ var score = document.getElementsByClassName("score");
 var initials = document.getElementsByClassName("user-input");
 
 //the all done screen has a button labeled <enter initals> and the score and initials provided by the user should be saved to the object which will populate data in the highscore table
-var saveButton = document.getElementById("#save-data");
+var saveButton = document.getElementById("save-data");
 
 //the highscore screen can be entered from the title page or is displayed after the all done screen has been handled by the user - this screen is always in a game over state it has a table which will display data after calling the highscore object which saves all highscore/initial pairs entered
-var tds = document.querySelectorAll("td")
+var tds = document.querySelectorAll("td");
 
 //also on the highscore screen is a button labeled clear all data which clears the table and presumably the data stored in the high score object
 var clear = document.getElementById("#clear-all");
+
+
+//timer logic SELECT and MANIPULATE
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+
+    var secondsLeft = 60;
+
+    function setTime() {
+      var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = "timer: " + secondsLeft ;
+    
+        if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+          sendMessage();
+        }
+    
+      }, 1000);
+    }
+    function sendMessage() {
+      timeEl.textContent = " ";
+    
+      var gameOver = document.createElement("gameover");
+      gameOver.textContent = "GAME OVER!"  
+    }
+    setTime();
+
+
+
+var quizQuestions = [
+    {
+        question: "Why do JavaScript and Java have similar names?",
+        answers: {
+          a:  "JavaScript is a stripped-down version of Java", 
+          b:  "JavaScript's syntax is loosely based on Java's",
+          c: "They both originated on the island of Java" 
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
+        answers: {
+            a: "The User's machine running a Web browser", 
+            b: "The Web server",
+            c: "central machine deep within Netscape's corporate offices"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: " ______ JavaScript is also called client-side JavaScript",
+        answers: {
+            a: "Microsoft", 
+            b: "Navigator",
+            c: "LiveWire"
+        },
+        correctAnswer: "b"
+
+    },
+    {
+        question: " What are variables used for in JavaScript Programs?",
+        answers: {
+            a: " Storing numbers, dates, or other values", 
+            b: "Varying randomly",
+            c: "Causing high-school algebra flashbacks"
+        },
+        correctAnswer: "a"
+
+    },
+    {
+        question: " What is the correct JavaScript syntax to write 'Hello World'?",
+        answers: {
+            choice1: " System.out.println('Hello World')", 
+            choice2: "println ('Hello World')",
+            choice3: "document.write('Hello World')" 
+        },
+        correctAnswer: "c"
+
+    },
+
+
+
+]
+
+
