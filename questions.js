56aca5cb2c81071a04c3c536b9dd8ce197bd14c7
Claudia Petty <claudialove32@gmail.com>
2020-04-10 22:01:19 -0400

    i tried to push my final homework submission and it didnt work, trying again

---
+++ questions.js
@@ -0,0 +1,32 @@
+      //questions array
+      var questions = [{
+          "question": "Who invented JavaScript?",
+            "option1": "Douglas Crockford",
+            "option2": "Sheryl Sandberg",
+            "option3": "Brendan Eich",
+             "answer": "3"
+        },{
+            "question": "Which of the following function of Array object removes the first element from an array and returns that element?",
+            "option1": "reverse()",
+            "option2": "shift()",
+            "option3": "slice()",
+             "answer": "2"
+        },{
+            "question": "Which of the following type of variable is visible everywhere in your JavaScript code?",
+            "option1": "global variable",
+            "option2": "local variable",
+            "option3": "both answers are correct",
+             "answer": "1"
+        },{
+            "question": "Which built-in method combines the text of two strings and returns a new string?",
+            "option1": "append()",
+            "option2": "concat()",
+            "option3": "attach()",
+             "answer": "2"
+        },{
+            "question": "What is the HTML tag under which one can write the JavaScript code?",
+            "option1": "<javascript>",
+            "option2": "<scripted>",
+            "option3": "<script>",
+             "answer": "3"}
+      ];
--- quiz.js
+++ quiz.js
@@ -1,28 +1,3 @@
-//connecting javascript and html to create intended effects requires knowing how to SELECT elements and MANIPULATE them with logic.  The following elements will need to be SLECTED in order to be MANIPULATED
-
-
-//play game screen this element changes text to display each of the questions in the array that will be written containting a nested object for each question with a questions and a correct answer
-var question = document.getElementById("question");
-
-//on play game screen - each #question has a list of multiple choice (li tags): one is correct and 2 are wrong - this element will need to change with hover over, and trigger and event to decide correct or incorrect and loop to the next question or end game if all questions have been looped through
-var lis = document.querySelectorAll("li");
-
-//on the all done screen the game is back in game over mode and this screen indicates the user answered all questions before the timer ran out - this screen has a span - the logic will have to store the score in an object and return this value to display in the span that starts as 0
-var score = document.getElementsByClassName("score");
-
-//the all done screen also has a form with a text box for user input - the value entered here needs to be stored and returned later when the highscore screen is displayed
-var initials = document.getElementsByClassName("user-input");
-
-//the all done screen has a button labeled <enter initals> and the score and initials provided by the user should be saved to the object which will populate data in the highscore table
-var saveButton = document.getElementById("save-data");
-
-//the highscore screen can be entered from the title page or is displayed after the all done screen has been handled by the user - this screen is always in a game over state it has a table which will display data after calling the highscore object which saves all highscore/initial pairs entered
-var tds = document.querySelectorAll("td");
-
-//also on the highscore screen is a button labeled clear all data which clears the table and presumably the data stored in the high score object
-var clear = document.getElementById("#clear-all");
-
-
 //timer logic SELECT and MANIPULATE
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
@@ -42,65 +17,65 @@ var mainEl = document.getElementById("main");
       }, 1000);
     }
     function sendMessage() {
-        var gameOver = document.createElement("div");
-        gameOver.innerHTML = "<p>GAME OVER!</p>";
+      //game over displays but doesnt not stop game play
+        timeEl.textContent = "Game Over! ";
+      };
+      //timer counts down but does not decrement if a wrong answer is picked - i work too slowly and had to choose to leave this out
+      setTime();
+
+
+//declare variables to select
+var currentQuestion = 0;
+var score = 0;
+var totQuestions = questions.length;
+
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
+
+
+   //see question.js file for question array
+    
+    //questions logic loop through
+      function loadQuestion (questionIndex) {
+        var q = questions[questionIndex];
+        questionEl.textContent = (questionIndex + 1) + '. ' + q.question; 
+        opt1.textContent = q.option1;
+        opt2.textContent = q.option2;
+        opt3.textContent = q.option3;
+       };
+   
+       function loadNextQuestion() {
+           var selectedOption = document.querySelector('input[type=radio]:checked');
+           if(!selectedOption) {
+               alert('Please select your answer!');
+               return;
+           }
+           var answer = selectedOption.value;
+           if(questions[currentQuestion].answer == answer) {
+           score += 10;
+          //worked hard and still didnt get to adding "correct" or "wrong" answer in game play
+       }
+       selectedOption.checked = false;
+       currentQuestion++;
+       if (currentQuestion == totQuestions - 1) {
+           nextButton.textContent = 'Finish';
+       }
+       if (currentQuestion == totQuestions) {
+           container.style.display = 'none';
+           resultCont.style.display = '';
+           resultCont.textContent = 'YourScore: ' + score;
+           return;
+
+       }
+       loadQuestion(currentQuestion);
     }
-    setTime();
 
+loadQuestion(currentQuestion);
 
-
-var quizQuestions = [
-    {
-        question: "Why do JavaScript and Java have similar names?",
-        answers: {
-          a:  "JavaScript is a stripped-down version of Java", 
-          b:  "JavaScript's syntax is loosely based on Java's",
-          c: "They both originated on the island of Java" 
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "When a user views a page containing a JavaScript program, which machine actually executes the script?",
-        answers: {
-            a: "The User's machine running a Web browser", 
-            b: "The Web server",
-            c: "central machine deep within Netscape's corporate offices"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: " ______ JavaScript is also called client-side JavaScript",
-        answers: {
-            a: "Microsoft", 
-            b: "Navigator",
-            c: "LiveWire"
-        },
-        correctAnswer: "b"
-
-    },
-    {
-        question: " What are variables used for in JavaScript Programs?",
-        answers: {
-            a: " Storing numbers, dates, or other values", 
-            b: "Varying randomly",
-            c: "Causing high-school algebra flashbacks"
-        },
-        correctAnswer: "a"
-
-    },
-    {
-        question: " What is the correct JavaScript syntax to write 'Hello World'?",
-        answers: {
-            choice1: " System.out.println('Hello World')", 
-            choice2: "println ('Hello World')",
-            choice3: "document.write('Hello World')" 
-        },
-        correctAnswer: "c"
-
-    },
-
-
-
-]
-
-
+//there should be logic to store and handle the highscore table that can be seen, but again even with working around the clock i didnt get to it
+    
